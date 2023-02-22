ee8bd6ced8febc3b2eadb00253cb722c70e6e489
Saeed Gholami <saeed.ghollami@gmail.com>
2023-02-22 20:53:53 +0330

    Add contact form and function.

--- src/v6/ui.py
+++ src/v6/ui.py
@@ -56,26 +56,58 @@ class ContactFrame(ttk.Frame):
             contact_id, contact, phone = contact
             contact_row = (index, contact, phone)
             self.contact_tree.insert("", "end", iid=contact_id, values=contact_row)
+        conn.close()
 
 
 class ActionFrame(ttk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        self.add_button = ttk.Button(self, text="Add Contact")
-        self.add_button.pack(fill=tk.BOTH, expand=True)
+        self.conn = phonebook.create_connection("phonebook.db")
+        self.name_var = tk.StringVar()
+        self.phone_var = tk.StringVar()
+        self.add_button = ttk.Button(self, text="Add Contact", command=self.add_contact)
+        self.edit_contact = ttk.Button(self, text="Edit Contact")
+        self.delete_contact = ttk.Button(self, text="Delete Contact")
+        self.add_button.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+        self.edit_contact.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+        self.delete_contact.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+
+    def add_contact(self):
+        toplevel = tk.Toplevel()
+        toplevel.title("Add Contact")
+        frame = ttk.Frame(toplevel, padding=(5, 10))
+        frame.pack(fill=tk.BOTH, expand=True)
+        ttk.Label(frame, text="Name: ").pack(fill=tk.BOTH, expand=True)
+        name_entry = ttk.Entry(frame, textvariable=self.name_var)
+        name_entry.pack(fill=tk.BOTH, expand=True)
+        ttk.Label(frame, text="Phone: ").pack(fill=tk.BOTH, expand=True)
+        phone_entry = ttk.Entry(frame, textvariable=self.phone_var)
+        phone_entry.pack(fill=tk.BOTH, expand=True)
+        save_button = ttk.Button(frame, text="Save", command=self.save_contact)
+        save_button.pack(fill=tk.BOTH, expand=True, pady=(10, 5))
+        close_button = ttk.Button(frame, text="Close", command=lambda: toplevel.destroy)
+        close_button.pack(fill=tk.BOTH, expand=True)
+
+    def save_contact(self):
+        contact = (self.name_var.get(), self.phone_var.get())
+        phonebook.add_contact(self.conn, contact)
+        msgbox.showinfo("Save Contact", "Contact save successfully.")
+        # Clear name and phone entry
+        self.name_var.set("")
+        self.phone_var.set("")
 
 
 class MainFrame(ttk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         # search frame
-        search_frame = SearchFrame(self)
+        search_frame = SearchFrame(self, padding=(5, 10))
         search_frame.grid(row=0, column=0, sticky=HV)
         # Contact Frame
-        contact_frame = ContactFrame(self)
+        contact_frame = ContactFrame(self, padding=(5, 10))
         contact_frame.grid(row=1, column=0, sticky=HV)
         # Action frame
-        action_frame = ActionFrame(self)
+        action_frame = ActionFrame(self, padding=(5, 10))
         action_frame.grid(row=2, column=0, sticky=HV)
         # row and column configure
         self.columnconfigure(0, weight=1)
